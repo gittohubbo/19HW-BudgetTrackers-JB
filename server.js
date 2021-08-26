f5dfe826403d9cd8b1753f17bcc06ced97dd1656
Adam Jones <jbensus@gmail.com>
2021-08-26 18:03:28 -0500

    deploying 2

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/${process.env.database}`, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/database', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
